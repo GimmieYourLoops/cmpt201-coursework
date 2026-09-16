49e28acc39e86c4a2db5b0cabb172687d7e60fb3
Lucian <lyy11@sfu.ca>
2026-09-15 17:32:21 -0700

    Checking for if i need to compile

--- lab0/lab0.c
+++ lab0/lab0.c
@@ -4,4 +4,5 @@ int main() {
   printf("Hello world!\n");
   printf("I don't know how to program in C but we ball frfr\n");
   printf("pp\n");
+  printf("Did this work, do i need to compile, idk\n");
 }
