353e86f91ebae1394817a4d4b3f250b67740f319
Lucian <lyy11@sfu.ca>
2026-09-15 17:39:37 -0700

    pp yuh

---
+++ lab0/pp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  printf("yung money clan\n");
+  int sum = 0;
+  for (int i = 0; i < 6; i++) {
+    sum += 2;
+  }
+  printf("%d\n", sum);
+}
